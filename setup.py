553bb8acefb171e9b7d29e2b097012f0367da23c
Alexandru Vasilache <36196612+Alex-Vasilache@users.noreply.github.com>
2024-11-12 14:39:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,4 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=requirements,
-    # Optional metadata
-    author='FZI',
-    author_email='nitzsche@fzi.de',
-    description='Spike Encoding and Data Utilities',
-    url='https://essgitlab.fzi.de/ecs-neuromorphic-demo/spike-encoding'
 )
